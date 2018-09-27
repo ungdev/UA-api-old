76c3785577b6aa4fa57c1c443a988a1d36932abb
Arnaud9145 <arnaud.dufour@utt.fr>
2018-09-27 10:50:23 +0200

    added log.info

--- src/env.js
+++ src/env.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const dotenv = require('dotenv')
 
+const log = require('./api/utils/log')(module)
+log.info(`Process env variables : ${process.env.ARENA_DB_TYPE}://${process.env.ARENA_DB_USER}:PASSWORD@${process.env.ARENA_DB_HOST}:${process.env.ARENA_DB_PORT}/${process.env.ARENA_DB_NAME}`)
+
+
 const defaultConfig = dotenv.config({
   path: path.join(__dirname, '..', '.env')
 })
