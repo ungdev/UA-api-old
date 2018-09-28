b10d19dbb81289c3dcd6e569bb9840c4f0ad039e
Arnaud9145 <arnaud.dufour@utt.fr>
2018-09-28 16:55:19 +0200

    added delete info endpoint, added "deleted" to infos

--- src/api/models/info.js
+++ src/api/models/info.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('info', {
     title: { type: DataTypes.STRING },
-    content: { type: DataTypes.STRING }
+    content: { type: DataTypes.STRING },
+    deleted: { type: DataTypes.BOOLEAN, defaultValue: false }
   })
 }
