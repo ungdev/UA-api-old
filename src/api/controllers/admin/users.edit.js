f9ec83cb455ce4d6c1701d7da149c12845072be7
Arnaud9145 <arnaud.dufour@utt.fr>
2018-09-29 22:53:55 +0200

    added route to modify isAdmin

---
+++ src/api/controllers/admin/users.edit.js
@@ -0,0 +1,30 @@
+const isAdmin = require('../../middlewares/isAdmin')
+const errorHandler = require('../../utils/errorHandler')
+
+/**
+ * put /users/id
+ *
+ * Response:
+ * 
+ */
+module.exports = app => {
+  app.put('/users/:id', [isAdmin()])
+
+  app.put('/users/:id', async (req, res) => {
+    const { User } = req.app.locals.models
+
+    try {
+      let user = await User.findById(req.params.id)
+      if(req.body.isAdmin !== null){
+        req.body.isAdmin === 100 ? user.isAdmin = 100 : user.isAdmin = 0
+      }
+
+      await user.save()
+      return res
+        .status(200)
+        .end()
+    } catch (err) {
+      errorHandler(err, res)
+    }
+  })
+}
