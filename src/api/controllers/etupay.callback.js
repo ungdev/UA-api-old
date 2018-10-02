1e1ed409fa6a99dd38b464889d0b2668dd6ac2d4
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-02 17:10:52 +0200

    removed some log, handled payment error

--- src/api/controllers/etupay.callback.js
+++ src/api/controllers/etupay.callback.js
@@ -22,7 +22,7 @@ async function handlePaylod(User, payload) {
     user.transactionId = payload.transactionId
     user.transactionState = payload.step
     user.paid = payload.paid
-    user.paid_at = new Date()
+    if(user.paid) user.paid_at = new Date()
 
     log.info(`user ${user.name} is at state ${user.transactionState}`)
 
@@ -52,15 +52,13 @@ async function handlePaylod(User, payload) {
  */
 module.exports = app => {
   app.post('/user/pay/callback', etupay.middleware, async (req, res) => {
-    log.info('callback')
-    log.info('req.etupay')
-    log.info(req.etupay ? 'req.etupay pas undefined !!' : 'undefined :(')
+    log.info('/user/pay/callback')
     const { shouldSendMail, user } = await handlePaylod(req.app.locals.models.User, req.etupay)
     log.info('shouldSendMail', shouldSendMail)
 
     if (shouldSendMail) {
       //await sendPdf(user)
-      log.info('SEND SMS TO USER') //todo
+      log.info('SEND MAIL TO USER') //todo
     }
 
     return res
@@ -70,16 +68,15 @@ module.exports = app => {
   })
 
   app.get('/user/pay/return', etupay.middleware, async (req, res, next) => {
-    log.info('return')
-    log.info('req.etupay')
-    log.info(req.etupay ? 'req.etupay pas undefined !!' : 'undefined :(')
+    log.info('/user/pay/return')
     if (req.query.payload) {
       const { shouldSendMail, user } = await handlePaylod(req.app.locals.models.User, req.etupay)
       log.info('shouldSendMail', shouldSendMail)
       if (shouldSendMail) {
         //await sendPdf(user)
-        log.info('SEND SMS TO USER') //todo
+        log.info('SEND MAIL TO USER') //todo
       }
+      if(user.transactionState === 'refused') return res.redirect(env.ARENA_ETUPAY_ERRORURL)
       log.info(`redirect to ${env.ARENA_ETUPAY_SUCCESSURL}`)
       return res.redirect(env.ARENA_ETUPAY_SUCCESSURL)
       //return res.redirect(env.ARENA_ETUPAY_ERRORURL)
