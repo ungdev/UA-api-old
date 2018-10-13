9746c8531aa69473b55302580241465cabf5db7c
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-13 20:04:17 +0200

    removed dumb routes, changed pdf : added logos and tweeks

--- src/api/utils/sendPDF.js
+++ src/api/utils/sendPDF.js
@@ -29,9 +29,14 @@ const rogPath = path.join(__dirname, './', 'assets', 'asusrog.png')
 let rog = fs.readFileSync(rogPath, 'base64')
 rog = `data:image/png;base64,${rog}`
 
-const meltdownPath = path.join(__dirname, './', 'assets', 'meltdown.png')
-let meltdown = fs.readFileSync(meltdownPath, 'base64')
-meltdown = `data:image/png;base64,${meltdown}`
+let meltdown = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'meltdown.png'), 'base64')}`
+let rc3 = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'rc3.jpg'), 'base64')}`
+let scoupImg = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'scoup.png'), 'base64')}`
+let silverstone = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'silverstone.png'), 'base64')}`
+let noctua = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'noctua.png'), 'base64')}`
+let compumsa = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'compumsa.png'), 'base64')}`
+let tekliss = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'tekliss.png'), 'base64')}`
+let tcm = `data:image/png;base64,${fs.readFileSync(path.join(__dirname, './', 'assets', 'tcm.png'), 'base64')}`
 
 const mailMessage = `Vous avez acheté votre place pour l'UTT Arena 2018
 Vous trouverez la place en format numérique en pièce jointe. Veuillez conserver la place : elle sera nécessaire pour entrer à l'UTT Arena.
@@ -64,18 +69,41 @@ function generatePdf(user, barcode) {
 
     const doc = new PDFDocument()
 
-    doc.image(logo, (doc.page.width - 350) / 2, 30, { width: 350, height: 350 })
-
+    doc.image(logo, (doc.page.width - 277) / 2, 20, { width: 277, height: 397 })
+    let scoup = ''
+    if(user.kaliento) scoup = `${scoup}, kaliento`
+    if(user.mouse) scoup = `${scoup}, souris`
+    if(user.keyboard) scoup = `${scoup}, clavier`
+    if(user.headset) scoup = `${scoup}, casque`
+    if(user.chair) scoup = `${scoup}, chaise gaming`
+    if(user.screen24) scoup = `${scoup}, écran 24"`
+    if(user.screen27) scoup = `${scoup}, écran 27"`
+    if(user.laptop) scoup = `${scoup}, PC portable`
+    if(user.gamingPC) scoup = `${scoup}, PC gaming`
+    if(user.streamingPC) scoup = `${scoup}, PC streaming`
+    if(scoup.length > 0) scoup = scoup.substr(2, scoup.length)
     doc
         .fontSize(20)
-        .text(`Nom: ${user.name}`, 20, 400)
-        .text(`T-Shirt: ${shirt[user.shirt || 'none']}`, 20, 420)
-        .text(`${user.plusone ? 'Accompagnateur' : 'Joueur'}`, 20, 440)
+        .text(`Nom: ${user.lastname}`, 30, 400)
+        .text(`Prénom: ${user.firstname}`, 30, 420)
+        .text(`T-Shirt: ${shirt[user.shirt || 'none']}`, 30, 440)
+        .text(`Câble 5m: ${user.ethernet ? 'oui': 'non'}`, 30, 460)
+        .text(`Câble 7m: ${user.ethernet7 ? 'oui': 'non'}`, 30, 480)
+        .text(`Matériel: ${scoup}`, 30, 500)
+        .text(`Tombola: ${user.tombola}`, 30, 520)
+        .text(`${user.plusone ? 'Place Accompagnateur' : 'Place Joueur'}`, 30, 540)
 
     //doc.image(barcode, 215, 500)
 
-    doc.image(rog, 70, 575, { width: 171, height: 35 })
-    doc.image(meltdown, 367, 575, { width: 66, height: 61 })
+    doc.image(rog, 480, 450, { width: 100 })
+    doc.image(meltdown, 260, 560, { width: 100 })
+    doc.image(scoupImg, 20, 580, { height: 80 })
+    doc.image(rc3, 370, 710, { height: 75 })
+    doc.image(noctua, 20, 670, { width: 100 })
+    doc.image(compumsa, 130, 670, { height: 50 })
+    doc.image(tekliss, 370, 560, { height: 60 })
+    doc.image(tcm, 400, 630, { height: 70 })
+    doc.image(silverstone, 130, 730, { height: 50 })
 
     const buffers = []
 
