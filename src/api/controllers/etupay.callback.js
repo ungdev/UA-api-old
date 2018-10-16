e5c8596f659c6dc63ffd150a27149f9b05efd7b1
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-16 10:18:23 +0200

    added kick of team when paying a visitor place

--- src/api/controllers/etupay.callback.js
+++ src/api/controllers/etupay.callback.js
@@ -7,10 +7,31 @@ const etupay = require('@ung/node-etupay')({
   url: env.ARENA_ETUPAY_URL,
   key: env.ARENA_ETUPAY_KEY
 })
+async function leaveTeam(user) {
+  let team = { user }
+  if(user.team){
+    if (team.captainId === user.id) {
+      log.info(`user ${user.name} left ${team.name} and destroyed it, as captain`)
+      let users = await User.findAll({ where: { teamId: team.id } })
+      for(let u of users) {
+        u.joined_at = null
+        u.teamId = null
+        await u.save()
+      }
+      await team.destroy()
+    } else {
+      log.info(`User ${user.name} left his team because he paid a visitor place`)
+      user.joined_at = null
+      user.teamId = null
+      await user.save()
+      await team.removeUser(user.id)
+    }
+  }
 
-async function handlePaylod(User, payload) {
+}
+async function handlePaylod(User, Team, payload) {
   try {
-    const user = await User.findById(payload.serviceData)
+    const user = await User.findById(payload.serviceData, { include: [Team] })
 
 
     if (!user) {
@@ -25,6 +46,7 @@ async function handlePaylod(User, payload) {
     user.transactionState = payload.step
     user.paid = payload.paid
     if(user.paid) user.paid_at = moment().format()
+    if(user.plusone) await leaveTeam(user)
 
     log.info(`user ${user.name} is at state ${user.transactionState}`)
 
@@ -54,7 +76,7 @@ async function handlePaylod(User, payload) {
  */
 module.exports = app => {
   app.post('/user/pay/callback', etupay.middleware, async (req, res) => {
-    const { shouldSendMail, user } = await handlePaylod(req.app.locals.models.User, req.etupay)
+    const { shouldSendMail, user } = await handlePaylod(req.app.locals.models.User, req.app.locals.models.Team, req.etupay)
 
     if (shouldSendMail) {
       await sendPdf(user)
@@ -69,7 +91,7 @@ module.exports = app => {
 
   app.get('/user/pay/return', etupay.middleware, async (req, res, next) => {
     if (req.query.payload) {
-      const { shouldSendMail, user } = await handlePaylod(req.app.locals.models.User, req.etupay)
+      const { shouldSendMail, user } = await handlePaylod(req.app.locals.models.User, req.app.locals.models.Team, req.etupay)
       if (shouldSendMail) {
         await sendPdf(user)
         log.info('MAIL SENT TO USER') //todo
