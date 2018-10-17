a5e97d2eadc9246bf57aaa97d64c3a890b8bfcf0
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-17 21:19:57 +0200

    fixed empty list

--- src/api/controllers/admin/teamsInSpotlight.js
+++ src/api/controllers/admin/teamsInSpotlight.js
@@ -32,7 +32,7 @@ module.exports = app => {
           if(moment(teamCompletedAt).isBefore(user.paid_at)) teamCompletedAt = user.paid_at
           if(moment(teamCompletedAt).isBefore(user.joined_at)) teamCompletedAt = user.joined_at
         })
-        return {id: team.id, completed_at: teamCompletedAt, name: team.name}
+        return {id: team.id, completed_at: teamCompletedAt, name: team.name, users: team.users}
       }).sort((team1, team2) => moment(team1.completed_at).isAfter(team2.completed_at))
       .filter(team => isTeamFull(team, spotlight.perTeam, true))
 
