7b501fe66749db6eadce49570da3c1f751216434
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2018-10-20 15:17:45 +0200

    redirecting UA-Appli message to ua-global for test

--- src/api/controllers/slack/slack.post.js
+++ src/api/controllers/slack/slack.post.js
@@ -25,33 +25,33 @@ module.exports = app => {
           .status(400)
           .json({ error: "missing params" })
           .end()
-      let channel = ""
-      switch (req.body.toChannel) {
-        case "1":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_LOL
-          break
-        case "2":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_LOL
-          break
-        case "3":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_FORTNITE
-          break
-        case "4":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_CS
-          break
-        case "5":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_HS
-          break
-        case "6":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_SSBU
-          break
-        case "libre":
-          channel = env.SLACK_CHANNEL_UA_TOURNOI_LIBRE
-          break
-        default:
-          channel = env.SLACK_CHANNEL_UA_APP
-          break
-      }
+      let channel = "CDGH6PXMX"
+      // switch (req.body.toChannel) {
+      //   case "1":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_LOL
+      //     break
+      //   case "2":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_LOL
+      //     break
+      //   case "3":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_FORTNITE
+      //     break
+      //   case "4":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_CS
+      //     break
+      //   case "5":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_HS
+      //     break
+      //   case "6":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_SSBU
+      //     break
+      //   case "libre":
+      //     channel = env.SLACK_CHANNEL_UA_TOURNOI_LIBRE
+      //     break
+      //   default:
+      //     channel = env.SLACK_CHANNEL_UA_APP
+      //     break
+      // } 
       slack.post(
         channel,
         { text: req.body.message },
