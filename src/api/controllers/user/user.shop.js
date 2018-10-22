78c42a9143782207f7559fa2b15a8ae7ca3d1989
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-22 16:27:11 +0200

    remove check for other unpaid orders

--- src/api/controllers/user/user.shop.js
+++ src/api/controllers/user/user.shop.js
@@ -73,9 +73,6 @@ module.exports = app => {
 
   app.post('/user/shop', async (req, res) => {
     try {
-      let hasUnpaidOrder = await req.app.locals.models.Order.findOne({ where: { paid: 0 } })
-      log.info('HAS UNPAID ORDER', hasUnpaidOrder)
-      if(hasUnpaidOrder) return res.status(404).json({ error: 'UNPAID_ORDER' }).end()
       let order = {}
       order.ethernet = req.body.ethernet ? req.body.ethernet : false
       order.ethernet7 = req.body.ethernet7 ? req.body.ethernet7 : false
