9596944388d3af132ac85088e12f1110825a593f
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2018-11-26 20:15:54 +0100

    sanitizing

--- src/api/controllers/admin/admin.setRespo.js
+++ src/api/controllers/admin/admin.setRespo.js
@@ -1,15 +1,12 @@
 const isAdmin = require('../../middlewares/isAdmin')
 const isAuth = require('../../middlewares/isAuth')
 const errorHandler = require('../../utils/errorHandler')
-const _ = require ('lodash')
-const log = require('../../utils/log')(module)
-
 
 /**
  * PUT /admin/setrespo/:id
  *
  * Response: none
- * 
+ *
  */
 module.exports = app => {
   app.put('/admin/setrespo/:id', [isAuth(), isAdmin()])
@@ -18,28 +15,26 @@ module.exports = app => {
     const { Permission } = req.app.locals.models
 
     try {
-      if(req.body.admin === null) {
+      if (req.body.admin === null) {
         return res
-        .status(400)  // Bad request
-        .json({ error: 'BAD_REQUEST' })
-        .end()
-      }
-      else if(req.params.id === req.user.id) {
+          .status(400) // Bad request
+          .json({ error: 'BAD_REQUEST' })
+          .end()
+      } else if (req.params.id === req.user.id) {
         return res
-        .status(403)  // Forbidden
-        .json({ error: 'NOT_ALLOWED' })
-        .end()
+          .status(403) // Forbidden
+          .json({ error: 'NOT_ALLOWED' })
+          .end()
       }
 
       let permission = await Permission.find({
         where: { userId: req.params.id }
       })
 
-      if(permission) {
+      if (permission) {
         permission.respo = req.body.respo.toString()
         await permission.save()
-      }
-      else {
+      } else {
         await Permission.create({
           userId: req.params.id,
           admin: null,
@@ -47,11 +42,8 @@ module.exports = app => {
         })
       }
 
-      return res
-        .status(200)
-        .end()
-    }
-    catch (err) {
+      return res.status(200).end()
+    } catch (err) {
       errorHandler(err, res)
     }
   })
