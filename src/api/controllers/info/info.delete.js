6c064d499930b2db1da82667a2cbaca13bf2cc6c
Lyght <valentinpeltier@yahoo.fr>
2018-11-28 21:44:49 +0100

    Fix DELETE on /infos/:id/:infoId

--- src/api/controllers/info/info.delete.js
+++ src/api/controllers/info/info.delete.js
@@ -4,15 +4,23 @@ const isAuth = require('../../middlewares/isAuth')
 const log = require ('../../utils/log')(module)
 
 module.exports = app => {
-  app.delete('/infos/:id', [isAuth(), isRespo()])
-  app.delete('/infos/:id', async (req, res) => {
+  app.delete('/infos/:id/:infoId', [isAuth(), isRespo()])
+  app.delete('/infos/:id/:infoId', async (req, res) => {
     const { Info } = req.app.locals.models
 
     try {
-      log.info(JSON.stringify(req))
-      const info = await Info.findById(req.params.id)
+      const info = await Info.findById(req.params.infoId)
+
+      if(info.spotlightId !== req.params.id) {
+        return res
+          .status(401)
+          .json({error: "UNAUTHORIZED"})
+          .end()
+      }
+      
       info.deleted = true
       await info.save()
+
       return res
         .status(200)
         .json(info)
