1cf47e7e72f3428b7b5c0b28d129cf8a0cbf90ef
Arnaud9145 <arnaud.dufour@utt.fr>
2018-11-30 18:27:14 +0100

    added place validation

---
+++ src/api/controllers/validate/valide.place.js
@@ -0,0 +1,48 @@
+const errorHandler = require('../../utils/errorHandler')
+const isAdmin = require('../../middlewares/isAdmin')
+const isAuth = require('../../middlewares/isAuth')
+
+/**
+ * GET /admin/users
+ *
+ * Response:
+ * [
+ *    { id, name, firstname, lastname, email, paid, teamId, spotlightId, permission, orders }, ...
+ * ]
+ */
+module.exports = app => {
+  app.put('/validate', [isAuth(), isAdmin()]) // change isAdmin with permission
+
+  app.put('/validate', async (req, res) => {
+    const { User, Order } = req.app.locals.models
+
+    try {
+      if(!req.body.barcode && !req.body.name ) return res.status(400).json({ error: 'MISSING_PARAMS' })
+      let user = null
+      if (req.body.barcode) {
+        user = await User.findOne({
+          where: { barcode: req.body.barcode },
+          include: [{
+            model: Order,
+          }]
+        })
+      }
+      if (!user && req.body.name) {
+        user = await User.findOne({
+          where: { name: req.body.name },
+          include: [{
+            model: Order,
+          }]
+        })
+      }
+      if (!user) return res.status(404).json({ error: 'NOT_FOUND' })
+
+      return res
+        .status(200)
+        .json(user)
+        .end()
+    } catch (err) {
+      errorHandler(err, res)
+    }
+  })
+}
