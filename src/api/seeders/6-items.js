aaaefb4bd5dd5910f2a8dc40025f37b55fa21415
Valentin <valentinpeltier@yahoo.fr>
2019-10-07 14:52:21 +0200

    update items seeder

--- src/api/seeders/6-items.js
+++ src/api/seeders/6-items.js
@@ -15,13 +15,13 @@ module.exports = {
       stock: 40,
     }, {
       id: 3,
-      name: 'Câble ethernet (5M)',
+      name: 'Câble ethernet (5m)',
       key: 'ethernet5',
       price: 5,
       infos: 'Un câble (5m) est requis pour se brancher aux switchs des tables',
     }, {
       id: 4,
-      name: 'Câble ethernet (7M)',
+      name: 'Câble ethernet (7m)',
       key: 'ethernet7',
       price: 10,
       infos: 'Un câble (7m) plus long pour les joueurs situés en bout de table',
@@ -35,13 +35,13 @@ module.exports = {
       id: 6,
       name: 'T-shirt UA 2019 (Homme)',
       key: 'maleTshirt',
-      price: 15,
+      price: 13,
       infos: 'Un t-shirt souvenir de cette LAN de folie',
     }, {
       id: 7,
       name: 'T-shirt UA 2019 (Femme)',
       key: 'femaleTshirt',
-      price: 15,
+      price: 13,
       infos: 'Un t-shirt souvenir de cette LAN de folie',
     }, {
       id: 8,
