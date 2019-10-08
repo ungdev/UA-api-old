25234d2bcfb78d79fc224149235b4aad9d7e74e4
Valentin <valentinpeltier@yahoo.fr>
2019-10-08 18:23:13 +0200

    fix route GET /users, add OR operator

--- src/api/controllers/user/list.js
+++ src/api/controllers/user/list.js
@@ -7,6 +7,7 @@ const errorHandler = require('../../utils/errorHandler');
  * GET /users
  * Query Params: {
  *    exact: bool. Should the email or the username exactly match in the DB ?
+ *    or: bool. Should we search for matching email OR username ? Default operator is AND.
  *    email: String
  *    username: String
  * }
@@ -22,23 +23,34 @@ module.exports = (app) => {
   app.get('/users', async (req, res) => {
     const { User, Team, Tournament } = req.app.locals.models;
 
-    // Compute 'where' parameter
-    const reqWhere = {};
+    const exact = typeof req.query.exact !== 'undefined';
+    const or = typeof req.query.or !== 'undefined';
+
+    // Compute 'where' parameters
+    const reqWhere = [];
     if (req.query.email) {
-      reqWhere.email = typeof req.query.exact !== 'undefined'
-        ? req.query.email
-        : { [Op.like]: req.query.email ? `%${req.query.email}%` : '%%' };
+      reqWhere.push({
+        email: exact
+          ? req.query.email
+          : { [Op.like]: `%${req.query.email || ''}%` },
+      });
     }
     if (req.query.username) {
-      reqWhere.username = typeof req.query.exact !== 'undefined'
-        ? req.query.username
-        : { [Op.like]: req.query.username ? `%${req.query.username}%` : '%%' };
+      reqWhere.push({
+        username: exact
+          ? req.query.username
+          : { [Op.like]: `%${req.query.username || ''}%` },
+      });
     }
 
     try {
       const users = await User.findAll({
         attributes: ['username', 'firstname', 'lastname'],
-        where: Object.values(reqWhere).length > 0 ? { [Op.or]: [reqWhere] } : {},
+        where: Object.values(reqWhere).length > 0
+          ? {
+            [or ? Op.or : Op.and]: reqWhere,
+          }
+          : {},
         include: {
           model: Team,
           attributes: ['name'],
