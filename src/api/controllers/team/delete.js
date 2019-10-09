864b00b3bd767fd81f1e834b472fa2be4192ef8a
Florian Esslinger <florian.esslinger@live.fr>
2019-10-09 23:45:54 +0200

    fix: delete only if captain team, add isPaid in get team/:id

--- src/api/controllers/team/delete.js
+++ src/api/controllers/team/delete.js
@@ -12,9 +12,25 @@ module.exports = (app) => {
 
   app.delete('/teams/:id', async (req, res) => {
     const { Team } = req.app.locals.models;
-
+    if (req.params.id !== req.user.teamId) {
+      return res
+        .status(401)
+        .json({ error: 'UNAUTHORIZED' })
+        .end();
+    }
     try {
-      const team = await Team.findByPk(req.params.id);
+      const team = await Team.findOne({
+        where: {
+          id: req.params.id,
+          captainId: req.user.id,
+        },
+      });
+      if (!team) {
+        return res
+          .status(401)
+          .json({ error: 'NO_CAPTAIN' })
+          .end();
+      }
       await team.destroy();
       return res
         .status(204)
--- src/api/controllers/team/get.js
+++ src/api/controllers/team/get.js
@@ -1,6 +1,7 @@
 const isAuth = require('../../middlewares/isAuth');
 const errorHandler = require('../../utils/errorHandler');
 
+const ticketId = 1;
 /**
  * GET /teams/:id
  *
@@ -11,7 +12,7 @@ module.exports = (app) => {
   app.get('/teams/:id', [isAuth()]);
 
   app.get('/teams/:id', async (req, res) => {
-    const { Team, User, Tournament } = req.app.locals.models;
+    const { Team, User, Tournament, Cart, CartItem } = req.app.locals.models;
 
     try {
       const team = await Team.findOne({
@@ -29,11 +30,23 @@ module.exports = (app) => {
         where: { askingTeamId: req.params.id },
       });
       if (team) {
-        const users = team.users.map(
-          ({ username, firstname, lastname, email, id }) => (
-            { username, firstname, lastname, email, id }
-          ),
-        );
+        const users = await Promise.all(team.users.map(
+          async ({ username, firstname, lastname, email, id }) => {
+            const isCartPaid = await Cart.count({
+              where: {
+                transactionState: 'paid',
+              },
+              include: [{
+                model: CartItem,
+                where: {
+                  forUserId: id,
+                  itemId: ticketId,
+                },
+              }],
+            });
+            return ({ username, firstname, lastname, email, id, isPaid: !!isCartPaid });
+          },
+        ));
         askingUsers = askingUsers.map(
           ({ username, firstname, lastname, email, id }) => (
             { username, firstname, lastname, email, id }
