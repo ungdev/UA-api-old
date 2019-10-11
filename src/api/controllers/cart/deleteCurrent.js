448396142fda8c2b942fe78bc9214151c5f8d6a7
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-10-11 21:47:23 +0200

    Delete cart added

---
+++ src/api/controllers/cart/deleteCurrent.js
@@ -0,0 +1,42 @@
+const isAuth = require('../../middlewares/isAuth');
+const errorHandler = require('../../utils/errorHandler');
+
+/**
+ * DELETE /users/:userId/carts/current
+ * {
+ *
+ * }
+ * Response
+ * {
+ * }
+ */
+module.exports = (app) => {
+  app.get('/users/:userId/carts/current', isAuth());
+
+  app.get('/users/:userId/carts/current', async (req, res) => {
+    const { Cart } = req.app.locals.models;
+
+    try {
+      if (req.params.userId !== req.user.id) {
+        return res
+          .status(403)
+          .json({ error: 'UNAUTHORIZED' })
+          .end();
+      }
+
+      await Cart.destroy({
+        where: {
+          userId: req.params.userId,
+          transactionState: 'draft',
+        },
+      });
+
+      return res
+        .status(204)
+        .end();
+    }
+    catch (error) {
+      return errorHandler(error, res);
+    }
+  });
+};
