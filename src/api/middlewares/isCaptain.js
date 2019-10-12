48a5aec5afb8699447b47ddc3f35829e9e82e125
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-10-12 17:23:56 +0200

    Revert commit

--- src/api/middlewares/isCaptain.js
+++ src/api/middlewares/isCaptain.js
@@ -1,11 +1,11 @@
-module.exports = () => async (req, res, next) => {
+module.exports = () => (req, res, next) => {
   const { Team } = req.app.locals.models;
-  const userTeam = await Team.findByPk(req.user.teamId);
-
-  if (!userTeam // if user has no team
-    || req.params.teamId !== req.user.id // or param teamId doesn't match its teamId
-    || userTeam.captainId !== req.user.id // or he is not captain
-  ) {
+  const team = Team.findByPk(req.user.teamId, {
+    where: {
+      captainId: req.user.id,
+    },
+  });
+  if (!team) {
     return res
       .status(401)
       .json({ error: 'NO_CAPTAIN' })
