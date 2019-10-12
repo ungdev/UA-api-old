14c2d6a92fa77f6ab61ced1b10ac8f85377bf729
Florian Esslinger <florian.esslinger@live.fr>
2019-10-12 19:23:20 +0200

    fix: isCaptain

--- src/api/controllers/team/delete.js
+++ src/api/controllers/team/delete.js
@@ -12,29 +12,10 @@ module.exports = (app) => {
   app.delete('/teams/:id', [isAuth(), isCaptain(), isType('player')]);
 
   app.delete('/teams/:id', async (req, res) => {
-    const { Team } = req.app.locals.models;
-    if (req.params.id !== req.user.teamId) {
-      return res
-        .status(401)
-        .json({ error: 'UNAUTHORIZED' })
-        .end();
-    }
     try {
-      const team = await Team.findOne({
-        where: {
-          id: req.params.id,
-          captainId: req.user.id,
-        },
-      });
-      if (!team) {
-        return res
-          .status(401)
-          .json({ error: 'NO_CAPTAIN' })
-          .end();
-      }
       req.user.type = 'none';
       await req.user.save();
-      await team.destroy();
+      await req.team.destroy();
       return res
         .status(204)
         .end();
