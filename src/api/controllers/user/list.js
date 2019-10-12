ea3ceab5b3104062d8b223dfe4559429f2e95442
Valentin <valentinpeltier@yahoo.fr>
2019-10-12 03:51:13 +0200

    update GET /users, search is always in exact mode and only with email

--- src/api/controllers/user/list.js
+++ src/api/controllers/user/list.js
@@ -1,7 +1,8 @@
 const { Op } = require('sequelize');
+const { check } = require('express-validator');
 
+const validateBody = require('../../middlewares/validateBody');
 const isAuth = require('../../middlewares/isAuth');
-const isAdmin = require('../../middlewares/isAdmin');
 const errorHandler = require('../../utils/errorHandler');
 
 const ITEM_PLAYER_ID = 1;
@@ -10,10 +11,7 @@ const ITEM_VISITOR_ID = 2;
 /**
  * GET /users
  * Query Params: {
- *    exact: bool. Should the email or the username exactly match in the DB ?
- *    or: bool. Should we search for matching email OR username ? Default operator is AND.
  *    email: String
- *    username: String
  * }
  *
  * Response
@@ -22,39 +20,23 @@ const ITEM_VISITOR_ID = 2;
  * }
  */
 module.exports = (app) => {
-  app.get('/users', [isAuth(), isAdmin()]);
+  app.get('/users', [isAuth()]);
+
+  app.get('/users', [
+    check('email')
+      .isEmail(),
+    validateBody(),
+  ]);
 
   app.get('/users', async (req, res) => {
     const { User, Team, Tournament, Cart, CartItem } = req.app.locals.models;
 
-    const exact = typeof req.query.exact !== 'undefined';
-    const or = typeof req.query.or !== 'undefined';
-
-    // Compute 'where' parameters
-    const reqWhere = [];
-    if (req.query.email) {
-      reqWhere.push({
-        email: exact
-          ? req.query.email
-          : { [Op.like]: `%${req.query.email || ''}%` },
-      });
-    }
-    if (req.query.username) {
-      reqWhere.push({
-        username: exact
-          ? req.query.username
-          : { [Op.like]: `%${req.query.username || ''}%` },
-      });
-    }
-
     try {
-      const users = await User.findAll({
+      const user = await User.findOne({
         attributes: ['id', 'username', 'firstname', 'lastname', 'type'],
-        where: Object.values(reqWhere).length > 0
-          ? {
-            [or ? Op.or : Op.and]: reqWhere,
-          }
-          : {},
+        where: {
+          email: req.query.email,
+        },
         include: {
           model: Team,
           attributes: ['name'],
@@ -65,46 +47,51 @@ module.exports = (app) => {
         },
       });
 
-      if (users.length === 1) {
-        const isPaid = await Cart.count({
+      if (!user) {
+        return res
+          .status(404)
+          .json({ error: 'USER_NOT_FOUND' })
+          .end();
+      }
+
+      const isPaid = await Cart.count({
+        where: {
+          transactionState: 'paid',
+        },
+        include: [{
+          model: CartItem,
           where: {
-            transactionState: 'paid',
-          },
-          include: [{
-            model: CartItem,
-            where: {
-              forUserId: users[0].id,
-              itemId: {
-                [Op.in]: [ITEM_PLAYER_ID, ITEM_VISITOR_ID],
-              },
+            forUserId: user.id,
+            itemId: {
+              [Op.in]: [ITEM_PLAYER_ID, ITEM_VISITOR_ID],
             },
-          }],
-        });
-        const noTeam = users[0].team === null && users[0].type === 'player';
-        const isNone = users[0].type === 'none';
-        if (isPaid) {
-          return res
-            .status(400)
-            .json({ error: 'ALREADY_PAID' })
-            .end();
-        }
-        if (isNone) {
-          return res
-            .status(400)
-            .json({ error: 'NO_TYPE' })
-            .end();
-        }
-        if (noTeam) {
-          return res
-            .status(400)
-            .json({ error: 'NO_TEAM' })
-            .end();
-        }
+          },
+        }],
+      });
+      const noTeam = user.team === null && user.type === 'player';
+      const isNone = user.type === 'none';
+      if (isPaid) {
+        return res
+          .status(400)
+          .json({ error: 'ALREADY_PAID' })
+          .end();
+      }
+      if (isNone) {
+        return res
+          .status(400)
+          .json({ error: 'NO_TYPE' })
+          .end();
+      }
+      if (noTeam) {
+        return res
+          .status(400)
+          .json({ error: 'NO_TEAM' })
+          .end();
       }
 
       return res
         .status(200)
-        .json(users)
+        .json(user)
         .end();
     }
 
