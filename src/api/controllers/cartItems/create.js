36d404d04624c99d774d5844e2434619a3a3b492
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-11-07 01:56:34 +0100

    [Hotfix] Les stocks des t shirts hardcodé (#119)
    * Corrected security visitor flaw
    
    * Tshirt stocks added #hardcode

--- src/api/controllers/cartItems/create.js
+++ src/api/controllers/cartItems/create.js
@@ -5,6 +5,7 @@ const isAuth = require('../../middlewares/isAuth');
 const errorHandler = require('../../utils/errorHandler');
 const validateBody = require('../../middlewares/validateBody');
 const { isTournamentFull } = require('../../utils/isFull');
+const tshirtStocks = require('../../utils/tshirtStocks');
 
 /**
  * POST /carts/:cartId/cartItems
@@ -21,15 +22,15 @@ const { isTournamentFull } = require('../../utils/isFull');
  */
 const ITEM_PLAYER_ID = 1;
 const ITEM_VISITOR_ID = 2;
+const ITEM_SHIRT_MALE_ID = 6;
+const ITEM_SHIRT_FEMALE_ID = 7;
 
 module.exports = function createCartItem(app) {
   app.post('/carts/:cartId/cartItems', [isAuth()]);
 
   app.post('/carts/:cartId/cartItems', [
-    check('itemId')
-      .isInt(),
-    check('quantity')
-      .isInt(),
+    check('itemId').isInt(),
+    check('quantity').isInt(),
     check('attributeId')
       .optional()
       .isInt(),
@@ -81,16 +82,22 @@ module.exports = function createCartItem(app) {
       // Est-ce utile ?
       if (req.body.itemId === ITEM_PLAYER_ID) {
         const forUser = await User.findByPk(req.body.forUserId, {
-          include: [{
-            model: Team,
-            include: [{
-              model: Tournament,
-              include: [{
-                model: Team,
-                include: [User],
-              }],
-            }],
-          }],
+          include: [
+            {
+              model: Team,
+              include: [
+                {
+                  model: Tournament,
+                  include: [
+                    {
+                      model: Team,
+                      include: [User],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
         });
         const isFull = await isTournamentFull(forUser.team.tournament, req);
         if (isFull) {
@@ -101,7 +108,8 @@ module.exports = function createCartItem(app) {
         }
       }
 
-      if (req.body.itemId === ITEM_VISITOR_ID) {
+      const itemId = parseInt(req.body.itemId);
+      if (itemId === ITEM_VISITOR_ID) {
         const visitorItem = await Item.findByPk(ITEM_VISITOR_ID);
 
         const maxVisitors = visitorItem.stock;
@@ -110,21 +118,54 @@ module.exports = function createCartItem(app) {
           where: {
             itemId: ITEM_VISITOR_ID,
           },
-          include: [{
-            model: Cart,
-            attributes: [],
-            where: {
-              transactionState: {
-                [Op.in]: ['paid', 'draft'],
+          include: [
+            {
+              model: Cart,
+              attributes: [],
+              where: {
+                transactionState: {
+                  [Op.in]: ['paid', 'draft'],
+                },
               },
             },
-          }],
+          ],
         });
 
-        if (maxVisitors <= actualVisitors) {
+        if (maxVisitors < actualVisitors + req.body.quantity) {
           return res
             .status(400)
             .json({ error: 'VISITOR_FULL' })
+            .end();
+        }
+      }
+
+      if (itemId === ITEM_SHIRT_MALE_ID || itemId === ITEM_SHIRT_FEMALE_ID) {
+        const maxTShirt = tshirtStocks.find(
+          (stock) => stock.attributeId === req.body.attributeId && stock.itemId === req.body.itemId,
+        ).stock;
+
+        const actualTShirt = await CartItem.sum('quantity', {
+          where: {
+            itemId,
+            attributeId: req.body.attributeId,
+          },
+          include: [
+            {
+              model: Cart,
+              attributes: [],
+              where: {
+                transactionState: {
+                  [Op.in]: ['paid', 'draft'],
+                },
+              },
+            },
+          ],
+        });
+
+        if (maxTShirt < actualTShirt + req.body.quantity) {
+          return res
+            .status(400)
+            .json({ error: 'TSHIRT_FULL' })
             .end();
         }
       }
